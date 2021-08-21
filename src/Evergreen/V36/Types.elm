025cf874c0322c4b3e73c82c96105da74cb49c9d
James Carlson <jxxcarlson@gmail.com>
2021-08-21 12:17:18 -0400

    Preparing for v36

---
+++ src/Evergreen/V36/Types.elm
@@ -0,0 +1,132 @@
+module Evergreen.V36.Types exposing (..)
+
+import Browser
+import Browser.Dom
+import Browser.Navigation
+import Evergreen.V36.Authentication
+import Evergreen.V36.Data
+import Evergreen.V36.User
+import Http
+import Markdown.Render
+import Random
+import Time
+import Url
+
+
+type AppMode
+    = EntryMode
+    | EditMode
+
+
+type SnippetViewMode
+    = SnippetExpanded
+    | SnippetCollapsed
+
+
+type PopupWindow
+    = AdminPopup
+
+
+type PopupStatus
+    = PopupOpen PopupWindow
+    | PopupClosed
+
+
+type ViewMode
+    = SmallView
+    | LargeView
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , url : Url.Url
+    , message : String
+    , currentTime : Time.Posix
+    , randomSeed : Random.Seed
+    , appMode : AppMode
+    , users : List Evergreen.V36.User.User
+    , currentUser : Maybe Evergreen.V36.User.User
+    , inputUsername : String
+    , inputPassword : String
+    , snippetText : String
+    , snippets : List Evergreen.V36.Data.Datum
+    , currentSnippet : Maybe Evergreen.V36.Data.Datum
+    , inputSnippetFilter : String
+    , snippetViewMode : SnippetViewMode
+    , windowWidth : Int
+    , windowHeight : Int
+    , popupStatus : PopupStatus
+    , viewMode : ViewMode
+    }
+
+
+type alias BackendModel =
+    { message : String
+    , randomSeed : Random.Seed
+    , randomAtmosphericInt : Maybe Int
+    , currentTime : Time.Posix
+    , dataDict : Evergreen.V36.Data.DataDict
+    , authenticationDict : Evergreen.V36.Authentication.AuthenticationDict
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | GotViewport Browser.Dom.Viewport
+    | NoOpFrontendMsg
+    | FETick Time.Posix
+    | GotAtomsphericRandomNumberFE (Result Http.Error String)
+    | GotNewWindowDimensions Int Int
+    | ChangePopupStatus PopupStatus
+    | SignIn
+    | SignOut
+    | InputUsername String
+    | InputPassword String
+    | InputSnippet String
+    | SearchBy String
+    | StarSnippet
+    | Save
+    | Fetch
+    | Cancel
+    | EditItem Evergreen.V36.Data.Datum
+    | Delete
+    | InputSnippetFilter String
+    | ExpandContractItem Evergreen.V36.Data.Datum
+    | RandomOrder
+    | ModificationOrder
+    | RandomizedOrder (List Evergreen.V36.Data.Datum)
+    | MarkdownMsg Markdown.Render.MarkdownMsg
+    | ExportYaml
+    | ExpandContractView
+    | AdminRunTask
+    | GetUsers
+
+
+type alias Username =
+    String
+
+
+type ToBackend
+    = NoOpToBackend
+    | RunTask
+    | SendUsers
+    | SaveDatum Username Evergreen.V36.Data.Datum
+    | SendUserData Username
+    | UpdateDatum Username Evergreen.V36.Data.Datum
+    | DeleteSnippetFromStore Username Evergreen.V36.Data.DataId
+    | SignInOrSignUp String String
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | GotAtomsphericRandomNumber (Result Http.Error String)
+    | Tick Time.Posix
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | SendMessage String
+    | GotUsers (List Evergreen.V36.User.User)
+    | GotUserData (List Evergreen.V36.Data.Datum)
+    | SendUser Evergreen.V36.User.User
